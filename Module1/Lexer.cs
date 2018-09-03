5ad15cb01dfc1168ba14b312eb49d65d4f6b87cf
Iolanta Paly <iolanta.paly@gmail.com>
2018-09-03 13:17:28 +0300

    Add files via upload

--- Module1/Lexer.cs
+++ Module1/Lexer.cs
@@ -60,6 +60,7 @@ public class IntLexer : Lexer
         NextCh();
         if (currentCh == '+' || currentCh == '-')
         {
+            
             NextCh();
         }
 
@@ -88,13 +89,99 @@ public class IntLexer : Lexer
     }
 }
 
+public class IdLexer : Lexer
+{
+    protected System.Text.StringBuilder idString;
+
+    public IdLexer(string input) : base(input)
+    {
+        idString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        NextCh();
+        if (char.IsLetter(currentCh))
+            NextCh();
+        else
+            Error();
+
+        while (char.IsLetter(currentCh) || char.IsDigit(currentCh) || currentCh == '_')
+            NextCh();
+
+        if (currentCharValue != -1)
+            Error();
+
+        System.Console.WriteLine("Id id recognized");
+    }
+}
+
+public class IntLexer1 : Lexer
+{
+    protected System.Text.StringBuilder intString;
+
+    public IntLexer1(string input)
+        : base(input)
+    {
+        intString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        NextCh();
+        if (currentCh == '+' || currentCh == '-')
+            NextCh();
+
+        if (currentCh != '0' && char.IsDigit(currentCh))
+            NextCh();
+        else
+            Error();
+
+        while (char.IsDigit(currentCh))
+            NextCh();
+
+        if (currentCharValue != -1)
+            Error();
+
+        System.Console.WriteLine("Integer is recognized");
+    }
+}
+
+public class IntChLexer : Lexer
+{
+    protected System.Text.StringBuilder intString;
+
+    public IntChLexer(string input)
+        : base(input)
+    {
+        intString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        NextCh();
+        if (char.IsLetter(currentCh))
+            NextCh();
+        else
+            Error();
+
+        while (char.IsDigit(currentCh) || char.IsLetter(currentCh))
+            NextCh();
+
+        if (currentCharValue != -1)
+            Error();
+
+        System.Console.WriteLine("Digits and chars are recognized");
+    }
+}
+
 
 public class Program
 {
     public static void Main()
     {
-        string input = "154216";
-        Lexer L = new IntLexer(input);
+        string input = "wtr_56";
+        Lexer L = new IdLexer(input);
         try
         {
             L.Parse();
@@ -104,5 +191,61 @@ public class Program
             System.Console.WriteLine(e.Message);
         }
 
+        string input1 = "_666_";
+        Lexer L1 = new IdLexer(input1);
+        try
+        {
+            L1.Parse();
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+
+
+
+        string input2 = "0746";
+        Lexer L2 = new IntLexer1(input2);
+        try
+        {
+            L2.Parse();
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+
+        string input3 = "-123";
+        Lexer L3 = new IntLexer1(input3);
+        try
+        {
+            L3.Parse();
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+
+        string input4 = "3aaa";
+        Lexer L4 = new IntChLexer(input4);
+        try
+        {
+            L4.Parse();
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+
+        string input5 = "a4b7c";
+        Lexer L5 = new IntChLexer(input5);
+        try
+        {
+            L5.Parse();
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
     }
 }
