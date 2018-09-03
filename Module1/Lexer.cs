56df061d892efb52e3a2b74b30bec9f86bc30e50
Романовский Андрей Андреевич <romanovsky@sfedu.ru>
2018-09-03 13:15:52 +0300

    first tasks completed

--- Module1/Lexer.cs
+++ Module1/Lexer.cs
@@ -44,6 +44,8 @@ public class Lexer
     }
 }
 
+
+//TASK1
 public class IntLexer : Lexer
 {
 
@@ -57,13 +59,106 @@ public class IntLexer : Lexer
 
     public override void Parse()
     {
+        int number = 0;
+        bool isNegative = false;
+        NextCh();
+        if (currentCh == '+' || currentCh == '-')
+        {
+            isNegative = currentCh == '-';
+            NextCh();
+        }
+
+        if (char.IsDigit(currentCh))
+        {
+            number = int.Parse(currentCh.ToString());
+            System.Console.WriteLine("Number is: {0}", number);
+            NextCh();
+        }
+        else
+        {
+            Error();
+        }
+
+        while (char.IsDigit(currentCh))
+        {
+
+            number = number*10 + int.Parse(currentCh.ToString());
+            System.Console.WriteLine("Number is: {0}", number);
+            NextCh();
+        }
+
+
+        if (currentCharValue != -1) // StringReader вернет -1 в конце строки
+        {
+            Error();
+        }
+        if (isNegative)
+            number *= -1;
+
+        System.Console.WriteLine("Integer is recognized");
+        System.Console.WriteLine("Your integer is:{0}", number);
+    }
+}
+
+
+//TASK2
+public class IdentLexer : Lexer
+{
+
+    protected System.Text.StringBuilder intString;
+
+    public IdentLexer(string input)
+        : base(input)
+    {
+        intString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        NextCh();
+        if (char.IsLetter(currentCh))
+        {
+            NextCh();
+        }
+        else
+        {
+            Error();
+        }
+
+        while (char.IsDigit(currentCh) || char.IsLetter(currentCh) || currentCh == '_')
+            NextCh();
+
+        if (currentCharValue != -1) // StringReader вернет -1 в конце строки
+        {
+            Error();
+        }
+
+        System.Console.WriteLine("Identificator is recognized");
+    }
+}
+
+
+//TASK3
+public class IntNotZeroLexer : Lexer
+{
+
+    protected System.Text.StringBuilder intString;
+
+    public IntNotZeroLexer(string input)
+        : base(input)
+    {
+        intString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
         NextCh();
         if (currentCh == '+' || currentCh == '-')
         {
             NextCh();
         }
 
-        if (char.IsDigit(currentCh))
+        if (currentCh != '0' && char.IsDigit(currentCh))
         {
             NextCh();
         }
@@ -74,6 +169,7 @@ public class IntLexer : Lexer
 
         while (char.IsDigit(currentCh))
         {
+
             NextCh();
         }
 
@@ -84,25 +180,193 @@ public class IntLexer : Lexer
         }
 
         System.Console.WriteLine("Integer is recognized");
-
     }
 }
 
+//Task 4
+public class LetterDigitInterleave: Lexer
+{
 
+    protected System.Text.StringBuilder intString;
+
+    public LetterDigitInterleave(string input)
+        : base(input)
+    {
+        intString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        bool nextDigit = false;
+        NextCh();
+        if (char.IsLetter(currentCh))
+        {
+            NextCh();
+            nextDigit = true;
+        }
+        else
+        {
+            Error();
+        }
+
+        while (nextDigit && char.IsDigit(currentCh) || !nextDigit && char.IsLetter(currentCh))
+        {
+            nextDigit = !nextDigit;
+            NextCh();
+        }
+
+        if (currentCharValue != -1) // StringReader вернет -1 в конце строки
+        {
+            Error();
+        }
+
+        System.Console.WriteLine("Identificator is recognized");
+    }
+}
+
+//Task 5
+public class LettersSeparated : Lexer
+{
+
+    protected System.Text.StringBuilder intString;
+
+    public LettersSeparated(string input)
+        : base(input)
+    {
+        intString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        string chars = "";
+        bool lastReadSeparator = false;
+        NextCh();
+        if (char.IsLetter(currentCh))
+        {
+            chars += currentCh;
+            NextCh();
+        }
+        else
+        {
+            Error();
+        }
+
+        while (currentCharValue != -1)
+        {
+                if ((currentCh == ';' || currentCh == ',') && !lastReadSeparator)
+                {
+                    lastReadSeparator = true;
+                    NextCh();
+                }
+                else if (char.IsLetter(currentCh))
+                {
+                    lastReadSeparator = false;
+                    while (char.IsLetter(currentCh))
+                    {
+                        chars += currentCh;
+                        NextCh();
+                    }
+                }
+                else
+                    Error();
+        }
+
+        if (lastReadSeparator)
+        {
+            Error();
+        }
+
+        System.Console.WriteLine("Identificator is recognized");
+        System.Console.WriteLine("Chars: {0}", chars);
+    }
+}
 public class Program
 {
     public static void Main()
     {
-        string input = "154216";
-        Lexer L = new IntLexer(input);
+        //ints
+        string intPos = "154216";
+        string intNeg = "-154216";
+        Lexer L1 = new IntLexer(intPos);
+        Lexer L3 = new IntLexer("12x3");
+        Lexer L2 = new IntLexer(intNeg);
+        System.Console.WriteLine("task 1:");
         try
         {
-            L.Parse();
+            L1.Parse();
+            L2.Parse();
+            L3.Parse();
+            
         }
         catch (LexerException e)
         {
             System.Console.WriteLine(e.Message);
         }
 
+        //idents
+        string goodIdent = "abc_23d";
+        string badIdent = "123a";
+        Lexer LId1 = new IdentLexer(goodIdent);
+        Lexer LId2 = new IdentLexer(badIdent);
+        System.Console.WriteLine("task 2:");
+        try
+        {
+            LId1.Parse();
+            LId2.Parse();
+
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+
+        //not zeros
+        string goodInt = "154216";
+        string badInt = "054216";
+        Lexer NotZeroL1 = new IntNotZeroLexer(goodInt);
+        Lexer NotZeroL2 = new IntNotZeroLexer(badInt);
+        System.Console.WriteLine("task 3:");
+        try
+        {
+            NotZeroL1.Parse();
+            NotZeroL2.Parse();
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+
+
+        //interleaved
+        Lexer Interleave1 = new LetterDigitInterleave("a1b2c3");
+        Lexer Interleave2 = new LetterDigitInterleave("a1b2c33");
+        Lexer Interleave3 = new LetterDigitInterleave("a1b2c");
+        System.Console.WriteLine("task 4:");
+        try
+        {
+            Interleave1.Parse();
+            Interleave3.Parse();
+            Interleave2.Parse();
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+
+        //letters separated
+        Lexer Separated1 = new LettersSeparated("a,b;c");
+        Lexer Separated2 = new LettersSeparated("abc");
+        Lexer Separated3 = new LettersSeparated("a;bc,");
+        System.Console.WriteLine("task 4:");
+        try
+        {
+            Separated1.Parse();
+            Separated2.Parse();
+            Separated3.Parse();
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
     }
 }
