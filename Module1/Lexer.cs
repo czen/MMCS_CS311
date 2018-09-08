c4b2a5f060e9f41ee77033d3aad0e3ed00195fbe
CodeIsLie <ilya1757@gmail.com>
2018-09-08 08:39:43 +0300

    first four lexers

--- Module1/Lexer.cs
+++ Module1/Lexer.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class LexerException : System.Exception
 {
     public LexerException(string msg)
@@ -48,6 +50,8 @@ public class IntLexer : Lexer
 {
 
     protected System.Text.StringBuilder intString;
+    protected String numberString;
+    public int number;
 
     public IntLexer(string input)
         : base(input)
@@ -60,11 +64,13 @@ public class IntLexer : Lexer
         NextCh();
         if (currentCh == '+' || currentCh == '-')
         {
+            numberString += currentCh;
             NextCh();
         }
 
         if (char.IsDigit(currentCh))
         {
+            numberString += currentCh;
             NextCh();
         }
         else
@@ -74,6 +80,99 @@ public class IntLexer : Lexer
 
         while (char.IsDigit(currentCh))
         {
+            numberString += currentCh;
+            NextCh();
+        }
+
+
+        if (currentCharValue != -1) // StringReader вернет -1 в конце строки
+        {            
+            Error();
+        }
+
+        number = Convert.ToInt32(numberString);
+        System.Console.WriteLine("Integer is recognized " + number);
+
+    }
+}
+
+public class IdLexer : Lexer
+{
+
+    protected System.Text.StringBuilder idString;
+    protected String id;
+
+    public IdLexer(string input)
+        : base(input)
+    {
+        idString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        NextCh();
+
+        if (char.IsLetter(currentCh))
+        {
+            id += currentCh;
+            NextCh();
+        }
+        else
+        {
+            Error();
+        }
+
+        while (char.IsDigit(currentCh) || char.IsLetter(currentCh))
+        {
+            id += currentCh;
+            NextCh();
+        }
+
+        if (currentCharValue != -1)
+        {
+            Error();
+        }
+        
+        System.Console.WriteLine("id is recognized " + id);
+
+    }
+}
+
+public class IntZeroLexer : Lexer
+{
+
+    protected System.Text.StringBuilder intString;
+    protected String numberString;
+    public int number;
+
+    public IntZeroLexer(string input)
+        : base(input)
+    {
+        intString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        NextCh();
+        if (currentCh == '+' || currentCh == '-')
+        {
+            numberString += currentCh;
+            NextCh();
+        }
+
+        if (char.IsDigit(currentCh) && currentCh != '9')
+        {
+            numberString += currentCh;
+            NextCh();
+        }
+        else
+        {
+            Error();
+        }
+
+        while (char.IsDigit(currentCh))
+        {
+            numberString += currentCh;
             NextCh();
         }
 
@@ -83,17 +182,72 @@ public class IntLexer : Lexer
             Error();
         }
 
-        System.Console.WriteLine("Integer is recognized");
+        number = Convert.ToInt32(numberString);
+        System.Console.WriteLine("Integer without 0 recognized " + number);
 
     }
 }
 
+public class AlternateCharDigitLexer: Lexer
+{
+    protected System.Text.StringBuilder mesString;
+    protected String message;
+
+    public AlternateCharDigitLexer(string input)
+        : base(input)
+    {
+        mesString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        NextCh();
+
+        if (char.IsLetter(currentCh))
+        {
+            message += currentCh;
+            NextCh();
+        }
+        else
+        {
+            Error();
+        }
+        
+        while (true)
+        {
+            if (currentCharValue == -1)
+                break;
+
+            if (!char.IsDigit(currentCh)) {
+                Error();
+            }
+            message += currentCh;
+            NextCh();
+
+            if (currentCharValue == -1)
+                break;
+
+            if (!char.IsLetter(currentCh)){
+                Error();
+            }
+            message += currentCh;
+            NextCh();
+        }
+
+        if (currentCharValue != -1)
+        {
+            Error();
+        }
+
+        System.Console.WriteLine("string with alternate chars and digits is recognized " + message);
+    }
+}
 
 public class Program
 {
     public static void Main()
     {
-        string input = "154216";
+        string input = "-154216";
         Lexer L = new IntLexer(input);
         try
         {
@@ -103,6 +257,6 @@ public class Program
         {
             System.Console.WriteLine(e.Message);
         }
-
+        Console.ReadLine();
     }
 }
