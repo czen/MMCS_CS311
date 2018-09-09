49d646fd26718386ce79da74f62ba4965f937586
VadimKaryakin <karyakin.vadim@yandex.ru>
2018-09-09 22:26:33 +0300

    Add files via upload

---
+++ Lexer.cs
@@ -0,0 +1,114 @@
+public class LexerException : System.Exception
+{
+    public LexerException(string msg)
+        : base(msg)
+    {
+    }
+
+}
+
+public class Lexer
+{
+    protected int position;
+    protected char currentCh;       // очередной считанный символ
+    protected int currentCharValue; // целое значение очередного считанного символа
+    protected System.IO.StringReader inputReader;
+    protected string inputString;
+
+    public Lexer(string input)
+    {
+        inputReader = new System.IO.StringReader(input); //класс, который считывает из указанной строки
+        inputString = input;
+    }
+
+    public void Error()
+    {
+        System.Text.StringBuilder o = new System.Text.StringBuilder();
+        o.Append(inputString + '\n');
+        o.Append(new System.String(' ', position - 1) + "^\n");
+        o.AppendFormat("Error in symbol {0}", currentCh);
+        throw new LexerException(o.ToString());
+    }
+
+    protected void NextCh()
+    {
+        this.currentCharValue = this.inputReader.Read();
+        this.currentCh = (char)currentCharValue;
+        this.position += 1;
+    }
+
+    public virtual void Parse()
+    {
+
+    }
+}
+
+public class IntLexer : Lexer
+{
+
+    protected System.Text.StringBuilder intString;
+
+    public IntLexer(string input)
+        : base(input)
+    {
+        intString = new System.Text.StringBuilder();
+    }
+
+    public override void Parse()
+    {
+        NextCh();
+        if (currentCh == '+' || currentCh == '-')
+        {
+            NextCh();
+        }
+
+        if (char.IsDigit(currentCh))
+        {
+            NextCh();
+        }
+        else
+        {
+            Error();
+        }
+
+        while (char.IsDigit(currentCh))
+        {
+            NextCh();
+            if (currentCharValue == -1)
+            {
+                intString.Append(inputString);
+                inputString = System.Console.ReadLine();
+                inputReader = new System.IO.StringReader(inputString);
+                NextCh();
+            }
+        }
+
+
+        if(currentCh != '_')
+        {
+            Error();
+        }
+
+        System.Console.WriteLine("Integer is recognized");
+        System.Console.WriteLine(intString);
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = System.Console.ReadLine();
+        Lexer L = new IntLexer(input);
+        try
+        {
+            L.Parse();
+        }
+        catch (LexerException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+
+    }
+}
