10855ca7b1be9e5a252bbebbf2bbfd3adab06b88
Dmitry Volodin <mr.molkree@gmail.com>
2018-09-19 00:45:05 +0300

    Lab2 - complete

--- Module2/SimpleLangLexerTest/Program.cs
+++ Module2/SimpleLangLexerTest/Program.cs
@@ -11,10 +11,17 @@ namespace SimpleLangLexerTest
     {
         public static void Main()
         {
-            string fileContents = @"begin 
-id23 := 24;  
-cycle ; 2 id258 id29 ; 
-end";
+            string fileContents = @":begin line1
+id23 := 24;  line2
+cycle ; 2 id258, id29 ; line3
+1 + x - 3 / iend div 22 mod 3 or not true line4
+x += 1 y -= 9 z /= y y *= 8 line5
+1 < 2 > 0 <= 9 >= 6 = 6 <> 8 line6
+// 1 < 2 > 0 //<= 9 >= 6 = 6 <> 8 line7
+{comment} line8
+end
+//
+{}";
             TextReader inputReader = new StringReader(fileContents);
             Lexer l = new Lexer(inputReader);
             try
