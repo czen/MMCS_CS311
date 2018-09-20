d99d8a18fc1e69cfdb46f505b440e5d65038e99e
Kulishova Alexandra <sasha_kulishova@mail.ru>
2018-09-20 14:24:08 +0300

    Module 3 upd.

--- Module3/GPLexProject/mymain.cs
+++ Module3/GPLexProject/mymain.cs
@@ -5,10 +5,18 @@ using ScannerHelper;
 
 namespace Main
 {
+
     class mymain
     {
         static void Main(string[] args)
         {
+            int cnt_id = 0;//кол-во идентификаторов
+            int min_id_len = Int32.MaxValue, max_id_len = 0; //минимальная, максимальная длины идентификаторов
+            double avg_id_len = 0; //средняя длина идентификатора
+
+            int sum_int = 0; //сумма всех целых
+            double sum_d = 0; //сумма всех вещественных
+
             // Чтобы вещественные числа распознавались и отображались в формате 3.14 (а не 3,14 как в русской Culture)
             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
 
@@ -21,8 +29,39 @@ namespace Main
             int tok = 0;
             do {
                 tok = scanner.yylex();
+                if (tok == (int)Tok.ID)
+                {
+                    cnt_id++; //подсчитываем кол-во идентификаторов
+                    int len = scanner.yyleng;
+                    avg_id_len += len;
+
+                    if (len < min_id_len)
+                        min_id_len = len;
+
+                    if (len > max_id_len)
+                        max_id_len = len;
+                }
+
+                if (tok == (int)Tok.INUM)
+                    sum_int += Int32.Parse(scanner.yytext);
+
+                if (tok == (int)Tok.RNUM)
+                    sum_d += Double.Parse(scanner.yytext);
+
                 if (tok == (int)Tok.EOF)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("number of id: {0:D}", cnt_id);
+                    Console.WriteLine("average length of the id: {0:N}", avg_id_len / cnt_id);
+                    Console.WriteLine("min length of the id: {0:D}", min_id_len);
+                    Console.WriteLine("min length of the id: {0:D}", max_id_len);
+
+                    Console.WriteLine();
+                    Console.WriteLine("sum of int: {0:D}", sum_int);
+                    Console.WriteLine("sum of double: {0:N}", sum_d);
                     break;
+                }
+
                 Console.WriteLine(scanner.TokToString((Tok)tok));
             } while (true);
 
