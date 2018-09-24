8d9fd1c09bf75784652ebf019770a3a794dc003a
NikMozd <mozdorov@yandex.ru>
2018-09-24 17:43:29 +0300

    Модуль 3: сделаны обязательные задания

--- Module3/GPLexProject/mymain.cs
+++ Module3/GPLexProject/mymain.cs
@@ -18,13 +18,50 @@ namespace Main
 
             Scanner scanner = new Scanner(new FileStream(fname, FileMode.Open));
 
+            // Задание 2
+            int ID_count = 0;
+            int ID_sum_len = 0;
+            int ID_max_len = 0;
+            int ID_min_len = int.MaxValue;
+
+            // Задание 3
+            int INUM_sum = 0;
+            double RNUM_sum = 0;
+
             int tok = 0;
             do {
                 tok = scanner.yylex();
                 if (tok == (int)Tok.EOF)
                     break;
+
+                // Задание 2
+                if (tok == (int)Tok.ID)
+                {
+                    ++ID_count;
+                    int l = scanner.yytext.Length;
+                    ID_sum_len += l;
+                    if (l > ID_max_len)
+                        ID_max_len = l;
+                    if (l < ID_min_len)
+                        ID_min_len = l;
+                }
+
+                // Задание 3
+                else if (tok == (int)Tok.INUM)
+                    INUM_sum += scanner.LexValueInt;
+                else if (tok == (int)Tok.RNUM)
+                    RNUM_sum += scanner.LexValueDouble;
+
                 Console.WriteLine(scanner.TokToString((Tok)tok));
             } while (true);
+
+            Console.WriteLine("\nID_count = " + ID_count.ToString());
+            Console.WriteLine("ID_max_len = " + ID_max_len.ToString());
+            Console.WriteLine("ID_min_len = " + ID_min_len.ToString());
+            Console.WriteLine("ID_avg_len = " + 
+                (ID_sum_len / (double)ID_count).ToString());
+            Console.WriteLine("Integer sum = " + INUM_sum.ToString());
+            Console.WriteLine("Real sum = " + RNUM_sum.ToString());
 
             Console.ReadKey();
         }
