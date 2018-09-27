20d6e131b8628ef7e68b816d93b7ed5a7c9fed5c
AnastasiaKorch <smile_nastya24@mail.ru>
2018-09-27 22:40:15 +0300

    Все задания выполнены

--- Module3/GPLexProject/ScannerHelper.cs
+++ Module3/GPLexProject/ScannerHelper.cs
@@ -1,4 +1,8 @@
 ﻿namespace ScannerHelper
 {
-    public enum Tok { EOF = 0, ID, INUM, RNUM, COLON, SEMICOLON, ASSIGN, BEGIN, END, CYCLE };
+    public enum Tok
+    {
+        EOF = 0, ID, INUM, RNUM, COLON, SEMICOLON, ASSIGN, BEGIN, END, CYCLE,
+        STRING, COMMENT, COMMENT_BEGIN, COMMENT_END, ID2
+    };
 }
